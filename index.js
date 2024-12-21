89f525137760aa2bd556f7fd815cd0f38a02a955
RohanKashid <rohankashid25753@gmail.com>
2024-12-21 16:32:33 +0530

    Implement user management endpoints for REST API

--- index.js
+++ index.js
@@ -19,10 +19,35 @@ app.get('/users' , (req,res)=>{
     res.send(html)
 })
 
-app.get('/users/:id' , (req,res)=>{
+app
+.route('/api/users/:id')
+.get((req,res)=>{
     const id = req.params.id;
-    const user = users.filter((user) => user.id == id);
+    const user = users.find((user) => user.id == id);
     res.json(user)
+})
+.patch((req,res)=>{
+    // todo : edit the user with id
+  return   res.json({status : "pending"})
+})
+.delete((req,res)=>{
+    // todo : delete the user with id 
+    res.json({status : "pending"})
+})
+
+app.post('/api/users',(req,res)=>{
+    // todo : creat e new user
+    res.json({status : "pending"})
+})
+
+app.patch('/api/users/:id',(req,res)=>{
+    // todo : edit the user with id
+    res.json({status : "pending"})
+})
+
+app.delete('/api/users/:id',(req,res)=>{
+    // todo : delete the user with id 
+    res.json({status : "pending"})
 })
 
 
